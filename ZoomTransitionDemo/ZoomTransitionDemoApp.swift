e4f08a9f0c48fab443de4db42c3ea6c94c0ef99e
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-06-12 15:20:04 +0200

    Initial Commit

---
+++ ZoomTransitionDemo/ZoomTransitionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomTransitionDemoApp.swift
+//  ZoomTransitionDemo
+//
+//  Created by Juan Gilsanz Polo on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ZoomTransitionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
